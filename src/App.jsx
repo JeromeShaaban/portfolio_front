d8fdbc601eb0f397efe205379b4ad8f1d0087c13
JaddN7 <jerome.shaaban@gmail.com>
2021-07-21 20:53:32 +0200

    Burger menu done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
+import React, { useState } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import BackgroundStars from "./components/BackgroundStars"
 import Header from "./components/Header"
 import Homepage from "./components/Homepage"
 import Projects from "./components/Projects"
 import ContactInfos from "./components/ContactInfos"
+import MenuContext from "./contexts/MenuContext"
 
 function App() {
+  const [open, setOpen] = useState(false)
   return (
-    <Router>
-      <BackgroundStars />
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={ContactInfos} />
-      </Switch>
-    </Router>
+    <MenuContext.Provider value={{ open: open, setOpen: setOpen }}>
+      <Router>
+        <BackgroundStars />
+        <Header />
+        {!open && (
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={ContactInfos} />
+          </Switch>
+        )}
+      </Router>
+    </MenuContext.Provider>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import "../styles/header.scss"
+import Menu from "./Menu"
 
 const Header = () => {
   return (
     <div id="HeaderTitle">
+      <Menu />
       <span> Welcome to Jérôme Shaaban's Portfolio</span>
     </div>
   )
