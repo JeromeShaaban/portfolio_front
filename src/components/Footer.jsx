fcd9196550c591af2cdaf70489ca0d55113f9ec5
JaddN7 <jerome.shaaban@gmail.com>
2021-07-22 17:13:51 +0200

    First version of the footer done, and version alpha of the portfolio done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import "../styles/footer.scss"
 
 const Footer = () => {
   return (
     <div className="footerContainer">
-      <Link to="/contact">
-        <p className="footerText">Contact</p>
-      </Link>
-      <p className="footerText">&copy; 2021 Jérôme Shaaban</p>
+      <div>
+        <Link to="/contact">
+          <p className="footerText">Contact</p>
+        </Link>
+        <p className="footerText">&copy; 2021 Jérôme Shaaban</p>
+      </div>
     </div>
   )
 }
